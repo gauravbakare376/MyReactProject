99a25fd5a6256a61618f4d8e279129abbe7f3f7d
Gaurav bakare <gauravbakare376@gmail.com>
2026-01-18 07:21:14 +0530

    React folder is make

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+const App = () => {
+  console.log("gaurav")
+  console.log("Rohini")
+  return (
+    <>
+     
+      <div id="dad">
+        <h1>Helllo React js</h1>
+      </div>
+
+      <div id="kaka">
+        <h2>Hello </h2>
+      </div>
+    </>
+   
+  )
+}
+
+export default App
