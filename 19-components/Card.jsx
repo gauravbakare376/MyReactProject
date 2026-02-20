ec2755ff52f29bc8b42ab6d4a082220954058974
Gaurav bakare <gauravbakare376@gmail.com>
2026-02-20 10:47:56 +0530

    change log changes are commited.

---
+++ 19-components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+      <h1>Hi Rohini</h1>
+      <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum libero eveniet quae vel similique ea aspernatur in voluptatem? Officiis corrupti consectetur, sunt accusamus omnis exercitationem magnam unde enim quaerat dolores!</h2>
+    </div>
+  )
+}
+
+export default Card
